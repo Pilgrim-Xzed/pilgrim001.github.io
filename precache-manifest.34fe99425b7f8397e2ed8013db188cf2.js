e2870dbbb8cf9f4152fb72faa68b41c3d6976184
Pilgrim001 <saeedbello12@gmail.com>
2020-06-18 19:57:09 +0100

    Updates

---
+++ precache-manifest.34fe99425b7f8397e2ed8013db188cf2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbde223978908c354891d352da41ee9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d99e767065a621bf480",
+    "url": "/static/css/3.f9898e9a.chunk.css"
+  },
+  {
+    "revision": "b1bc89094c40deaaf946",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "a6ce09fec0c144ad02cf",
+    "url": "/static/css/main.aa0fc0ac.chunk.css"
+  },
+  {
+    "revision": "9ad4af93d67f2234054b",
+    "url": "/static/js/2.c4170ff9.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/static/js/2.c4170ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d99e767065a621bf480",
+    "url": "/static/js/3.a6ee383b.chunk.js"
+  },
+  {
+    "revision": "b1bc89094c40deaaf946",
+    "url": "/static/js/4.8e2ae644.chunk.js"
+  },
+  {
+    "revision": "a6ce09fec0c144ad02cf",
+    "url": "/static/js/main.6e0887ee.chunk.js"
+  },
+  {
+    "revision": "ab81a35457acd50f2f3e",
+    "url": "/static/js/runtime-main.3c3c0862.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "6c77f4736171f3aa0a91f6836a5e04db",
+    "url": "/static/media/company.6c77f473.jpg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "867527fda5fa75b37d483ffe0d41e4a1",
+    "url": "/static/media/nimc.867527fd.jpg"
+  },
+  {
+    "revision": "2f5d0b08fbb3ecba8680457ccd7ebe58",
+    "url": "/static/media/restmeal.2f5d0b08.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
